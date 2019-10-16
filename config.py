4b3834683306554ec1623a10c9c6d382a1b120dd
Tyler <ttimms@kent.edu>
2019-10-16 14:02:26 -0400

    db initialized with basic cutomer table

--- config.py
+++ config.py
@@ -1,4 +1,10 @@
 import os
+baseDir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
+    # Secret key used by flask-wtf to generate csrf token.
     SECRET_KEY = os.environ.get('SECRET_KEY')
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_PATH') or 'sqlite:///' + os.path.join(baseDir, 'PaceApp.db')
+    # Track modifications currently disabled to avoid overhead.
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
